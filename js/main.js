42e005132a0a2dc120b6d6804587a9e32d25e2c3
AlexItMaster <alex.it.master@gmail.com>
2021-12-23 18:02:49 +0200

    HomeWork-8
    done

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
 /** Подключение модулей */
-import { generateLogs } from './modules/logs/index.js';
-import { addingToArena } from './modules/game/layout.js';
-import { fight, definingChampions } from './modules/game/logic.js';
-import { player1, player2, createPlayer } from './modules/players/index.js';
-
-/** Ф-я зупускает игру */
-const init = () => {
-    const $formFight = document.querySelector('.control'); // обращение к форме
-
-    /** Создание игроков и добавление их на арену */
-    addingToArena(createPlayer(player1));
-    addingToArena(createPlayer(player2));
-
-    /** Вызов лога с приветствием о начале игры */
-    generateLogs('start', player1, player2);
-
-    /** Создание обработчика события клик по кнопке FIGHT */
-    $formFight.addEventListener('submit', function(e) {
-        e.preventDefault();
-
-        /** Начинаем бой */
-        fight(player1, player2);
-
-        /** Узнаем кто победил */
-        definingChampions();
-    });
-}
-
-init();
+import { game } from './modules/game/index.js';
+game.start();
